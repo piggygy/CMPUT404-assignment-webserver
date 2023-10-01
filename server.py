75d9eb3f112f1c1f0293d029b4ab5f27ed0c0963
Zhi Liu <zhi2@ualberta.ca>
2023-10-01 12:45:08 -0600

    copyright

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 #  coding: utf-8 
 import socketserver
 
-# Copyright 2023 Zhi Liu
+# Copyright 2023 Zhi Liu, Abram Hindle, Eddie Antonio Santos
 # 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
